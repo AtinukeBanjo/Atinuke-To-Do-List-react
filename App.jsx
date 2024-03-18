c8e0ab49e313f60d461a8b3dbad71d31511d56cd
Rufai Atinuke Ramota Banjo <157470263+AtinukeBanjo@users.noreply.github.com>
2024-03-18 11:34:07 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './component/todo.css'
+import Mytodo from './component/todo'
+
+function App() {
+
+  return (
+    <>
+    <Mytodo></Mytodo>
+    </>
+  )
+}
+
+export default App
